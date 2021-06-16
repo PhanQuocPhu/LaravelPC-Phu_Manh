6b7114dc7c714cbf557c7cd51cd67e78388717ac
PhanQuocPhu <phanquocphu1998@gmail.com>
2021-06-16 10:37:01 +0700

    fix bug header

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\OutBanner;
+use App\Models\SlideBanner;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
@@ -11,6 +13,15 @@ class FrontendController extends Controller
     public function __construct()
     {
         $categories = Category::where(['c_active'=>Category::STATUS_PUBLIC])->get();  
-        View::share('categories', $categories);
+        $outBanners = OutBanner::where(['ob_status' => OutBanner::STATUS_PUBLIC])->limit(5)->get();
+        $slideBanners = SlideBanner::where(['sb_status' => SlideBanner::STATUS_PUBLIC])->limit(8)->get();
+        $firstSb = $slideBanners->first();
+        $viewData = [
+            'categories' => $categories,
+            'outBanners' => $outBanners,
+            'firstSb' => $firstSb,
+            'slideBanners' => $slideBanners
+        ];
+        View::share($viewData);
     }
 }
